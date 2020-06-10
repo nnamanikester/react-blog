490e24db8b5fa1a1387d230f39f9cc229af07e3b
kescript <nnamanikester@gmail.com>
2020-06-10 11:58:29 +0100

    Initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router";
+
+const navLinks = [
+  { title: "Home", path: "/" },
+  { title: "Blog", path: "./blog" },
+  { title: "Contact Us", path: "./contact" },
+  { title: "Login", path: "./login" },
+];
+
+const Navigation = () => {
+  return (
+    <nav className="site-navigation">
+      <span>My React Blog</span>
+      <ul>
+        {navLinks.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link>{link.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
